5475242fa163a3b633b8b1b1c0a61d9ed5929ab5
Brian Andres Suhit <briansuhit000@gmail.com>
2024-02-25 20:29:14 -0300

    Added new level and win condition

---
+++ Assets/Scripts/Win and Lose/GameWinningController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class GameWinningController : MonoBehaviour
+{
+    [SerializeField] private GameObject gameWinningMenu;
+
+    [SerializeField] private string buttonToMenu = "add level name here";
+    [SerializeField] private string buttonToResetGame = "add level name here";
+    [SerializeField] private GameObject FirstButtonSelected;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameWinningMenu.SetActive(true);
+        }
+    }
+
+    public void ResetButton()
+    {
+        SceneManager.LoadScene(buttonToResetGame);
+        var eventSystem = EventSystem.current;
+        eventSystem.SetSelectedGameObject(FirstButtonSelected, new BaseEventData(eventSystem));
+    }
+
+    public void MenuButton()
+    {
+        SceneManager.LoadScene(buttonToMenu);
+    }
+}
